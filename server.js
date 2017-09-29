c44a01ec49be2e553fa61a924702bde3582d6efb
Phamtien <phamtien1709@gmail.com>
2017-09-29 09:57:35 +0700

    minihack

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const config = require('./config.json')
+
+const homeRouter = require('./HomeRouter');
+const askRouter = require('./AskRouter');
+const apiRouter = require('./ApiRouter');
+const questionRouter = require('./questionRouter');
+const gameRouter = require('./gameRouter')
+
+let app = express();
+
+mongoose.connect(config.connectionString, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('connect success');
+  }
+});
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use('/', homeRouter);
+app.use('/ask', askRouter);
+app.use('/api', apiRouter);
+app.use('/question', questionRouter);
+app.use('/game', gameRouter);
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(6969, () => {
+  console.log('Server is ready at port', config.port);
+});
